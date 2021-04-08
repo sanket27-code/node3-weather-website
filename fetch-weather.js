2f0b3e27c0992dcd35d703cc45209329f597b590
Sanket Ainchwar <2018bit033@sggs.ac.in>
2021-04-08 13:45:59 +0530

    changes done on  describtion in fetch_weather file.

--- fetch-weather.js
+++ fetch-weather.js
@@ -2,17 +2,17 @@ const request = require('postman-request');
 
 const fetch_weather = (location, callback) => {
     const current_url = `http://api.weatherstack.com/current?access_key=f2dbe8ed9d959d232be7c15d97807976&query=${location}`
-    request({ url: current_url, json:true }, (error, response) => {
+    request({ url: current_url, json:true }, (error, {body}) => {
         if (error) {
             callback({err:'something went wrong!'})
-        } else if (response.body.error) {
+        } else if (body.error) {
             callback({err:'Unable to fetch data. Please check your entered location...'})
         } else {
             callback({
-                location:`${response.body.location.name}, ${response.body.location.country}`,
-                latitude: response.body.location.lat,
-                longitude: response.body.location.lon,
-                des: `Temperature is ${response.body.current.temperature} degree. It is ${response.body.current.weather_descriptions[0]} and there is ${response.body.current.precip}% chances of rain.`
+                location:`${body.location.name}, ${body.location.country}`,
+                latitude: body.location.lat,
+                longitude: body.location.lon,
+                des: `Temperature is ${body.current.temperature} degree out. It is ${body.current.weather_descriptions[0]} and feels like ${body.current.feelslike} degree. Humidity is ${body.current.humidity}%.`
             })
         }
     })
